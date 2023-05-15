192ce208a1a98c3ff2382a195a6a8818048b64e1
akarshi19 <211b029@juetguna.in>
2023-05-15 10:02:01 +0530

    add Sign In connectivity

--- app/src/main/java/com/example/takeeateasy/MainActivity.java
+++ app/src/main/java/com/example/takeeateasy/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.takeeateasy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextSwitcher;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
 
-        //getSupportActionBar().hide();
+        login=findViewById(R.id.loginbtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignIn_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        register=findViewById(R.id.Regbtn);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignUp_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
