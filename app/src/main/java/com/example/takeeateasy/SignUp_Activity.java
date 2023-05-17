d8ffecbe129df8e9cebce18480c7e4cd65b4925d
akhil888binoy <akhil888binoy@gmail.com>
2023-05-17 15:52:51 +0530

    food item

--- app/src/main/java/com/example/takeeateasy/SignUp_Activity.java
+++ app/src/main/java/com/example/takeeateasy/SignUp_Activity.java
@@ -2,13 +2,27 @@ package com.example.takeeateasy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp_Activity extends AppCompatActivity {
 
+    Button regis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        regis=findViewById(R.id.button3);
+
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignUp_Activity.this, FoodItemsScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
